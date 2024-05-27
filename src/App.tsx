b492ebcd1a5a7a14f9e0f0c80aebe41f28517775
xna <xna00@qq.com>
2024-05-27 22:00:39 +0800

    Init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Button from "./components/Button";
+
+import.meta.glob;
+
+function App() {
+	const [count, setCount] = useState(0);
+	return (
+		<div>
+			<Button onClick={() => setCount(count + 1)}>{count}</Button>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,5 @@
+const Button = (props: React.JSX.IntrinsicElements["button"]) => (
+	<button {...props}></button>
+);
+
+export default Button;
